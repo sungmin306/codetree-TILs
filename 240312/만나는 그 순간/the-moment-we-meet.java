fb7b2da411dcf7946441f81390ce3a2ee9e15349
조성민 <101984130+sungmin306@users.noreply.github.com>
2024-03-12 15:56:04 +0900

    Title: 만나는 그 순간, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240312/만나는 그 순간/the-moment-we-meet.java
+++ 240312/만나는 그 순간/the-moment-we-meet.java
@@ -12,56 +12,35 @@ public class Main {
         int[] arr1 = new int[MAX_T];
         int[] arr2 = new int[MAX_T];
         int now = 0;
-        int index = 1;
+        int timeA = 1;
         for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             char d = st.nextToken().charAt(0);
             int t = Integer.parseInt(st.nextToken());
-            if(d == 'R') {
-                while(t --> 0) {
-                    now++;         
-                    arr1[index] = now;
-                    index++;
-                }
-            }
-            else if(d == 'L') {
-                while(t --> 0) {
-                    now--;
-                    arr1[index] = now;
-                    index++;
-                }
+            while(t --> 0) {
+                if(d == 'R') arr1[timeA] =  arr1[timeA - 1] + 1;
+                else arr1[timeA] =  arr2[timeA - 1] -1;
+                timeA++;
             }
         }
-        int timeA = index;
-
-        //System.out.println(Arrays.toString(arr1));
-        now = 0;
-        index = 1;
+        System.out.println(Arrays.toString(arr1));
+        int timeB = 1;
         for(int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             char d = st.nextToken().charAt(0);
             int t = Integer.parseInt(st.nextToken());
-            if(d == 'R') {
-                while(t --> 0) {
-                    now++;
-                    arr2[index] = now;
-                    index++;
-                }
-            }
-            else if(d == 'L') {
-                while(t --> 0) {
-                    now--;
-                    arr2[index] = now;
-                    index++;
-                }
+            while(t --> 0) {
+                if(d == 'R') arr2[timeB] =  arr1[timeB - 1] + 1;
+                else arr2[timeB] =  arr2[timeB - 1] -1;
+                timeB++;
             }
         }
 
         //System.out.println(Arrays.toString(arr2));
 
         int result = -1;
-        for(int i = 0; i < timeA; i++) {
-            if(arr1[i] != 0 && arr1[i] == arr2[i]) {
+        for(int i = 1; i < timeA; i++) {
+            if(arr1[i] == arr2[i]) {
                 result = i;
                 break;
             }    
