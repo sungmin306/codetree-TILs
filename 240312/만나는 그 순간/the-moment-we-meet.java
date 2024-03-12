4710aafa0d2d1332e048782e54acf6f59c216e5e
조성민 <101984130+sungmin306@users.noreply.github.com>
2024-03-12 15:57:28 +0900

    Title: 만나는 그 순간, Time: 151ms, Memory: 27MB, Status: Passed - CodeTree

--- 240312/만나는 그 순간/the-moment-we-meet.java
+++ 240312/만나는 그 순간/the-moment-we-meet.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static final int MAX_T = 2001;
+    public static final int MAX_T = 1000001;
     public static void main(String[] args)throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
