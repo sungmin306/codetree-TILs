6c77cb487a9616a967016312c06f6ccc94b92f45
조성민 <101984130+sungmin306@users.noreply.github.com>
2024-03-21 00:40:23 +0900

    Title: 문자에 따른 명령 2, Time: 86ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240321/문자에 따른 명령 2/text-based-commands2.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int[] dx = {0,1,0,-1};
+    public static int[] dy = {1,0,-1,0};
+    public static void main(String[] args)throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        int dir = 0;
+        int x = 0;
+        int y = 0;
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == 'L') {
+                dir = dir - 1;
+            }
+            else if(c == 'R') {
+                dir = dir + 1;
+            }
+            if (dir == 4) {
+                dir = 0;
+            }
+            if (dir == -1) {
+                dir = 3;
+            }
+            if(c == 'L') {
+                x = x + dx[dir];
+                y = y + dy[dir];
+            }
+            
+        }
+        System.out.println(x + " " + y);
+    }
+}
