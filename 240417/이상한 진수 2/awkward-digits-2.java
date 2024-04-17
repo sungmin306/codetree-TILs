31736c4bc6c7f8a88c574fdd710df7d38bcbc243
조성민 <101984130+sungmin306@users.noreply.github.com>
2024-04-17 11:37:33 +0900

    Title: 이상한 진수 2, Time: 81ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240417/이상한 진수 2/awkward-digits-2.java
+++ 240417/이상한 진수 2/awkward-digits-2.java
@@ -3,12 +3,13 @@ import java.io.*;
 
 public class Main {
     public static String s;
+    public static char[] arr;
 
     public static void main(String[] args)throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         s = br.readLine();
-        char[] arr = s.toCharArray();
+        arr = s.toCharArray();
         for(int i = 0; i < arr.length; i++) {
             if(arr[i] == '0') {
                 arr[i] = '1';
@@ -23,9 +24,22 @@ public class Main {
                 //System.out.println(sum);
             }
         }
-        System.out.println(sum);   
+        
+        System.out.println(calculator());   
+        
+    }
 
-        
-        
+    public static int calculator() {
+        if(arr.length == 1) {
+            return 0;
+        }
+        int sum = 0;
+        for(int i = 0 ; i < arr.length; i++) {
+            if(arr[i] == '1') {
+                sum += Math.pow(2, ((arr.length) - i - 1));
+                //System.out.println(sum);
+            }
+        }
+        return sum;
     }
 }
