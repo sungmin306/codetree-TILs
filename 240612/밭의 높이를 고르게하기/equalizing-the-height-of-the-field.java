647c10a2a58aca46f3bbfc4541133b41c8a6b86e
조성민 <101984130+sungmin306@users.noreply.github.com>
2024-06-12 13:24:34 +0900

    Title: 밭의 높이를 고르게하기, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240612/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.java
+++ 240612/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.java
@@ -22,7 +22,7 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i = 0; i < N-H; i++) {
+        for(int i = 0; i < N-T; i++) {
             int temp = 0;
             for(int j = 0; j<T; j++) {
                 temp += Math.abs(H - arr[j+i]);
