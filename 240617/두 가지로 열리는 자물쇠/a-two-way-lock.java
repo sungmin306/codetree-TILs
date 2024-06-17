047f5fb1ee4d6b5ab606896a0237480db12d4522
조성민 <101984130+sungmin306@users.noreply.github.com>
2024-06-17 14:52:02 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 227ms, Memory: 10MB, Status: Passed - CodeTree

--- 240617/두 가지로 열리는 자물쇠/a-two-way-lock.java
+++ 240617/두 가지로 열리는 자물쇠/a-two-way-lock.java
@@ -1,70 +1,37 @@
-import java.util.*;
-import java.io.*;
-
+import java.util.Scanner;
 
 public class Main {
+    public static int n;
+    public static int a, b, c;
+    public static int a2, b2, c2;
 
-    public static int N;
-    public static int countfunc(int[] arr,ArrayList<int[]> arrayList) {
-        int count = 0;
-        for(int i = 0; i < N; i++) {
-            if((Math.abs(arr[0] - i) <= 2 || Math.abs(arr[0] - i) >= 8)) {
-                for(int j = 0; j < N; j++) {
-                    if((Math.abs(arr[1] - j) <= 2 || Math.abs(arr[1] - j) >= 8)) {
-                        for(int k = 0; k < N; k++) {
-                            if((Math.abs(arr[2] - k) <= 2 || Math.abs(arr[2] - k) >= 8)) {
-                                arrayList.add(new int[] {i,j,k});
-                                count++;
-                            }
-                        }
-                    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력
+        n = sc.nextInt();
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        a2 = sc.nextInt();
+        b2 = sc.nextInt();
+        c2 = sc.nextInt();
+        
+        // 모든 조합을 다 만들어 봅니다.
+        int cnt = 0;
+        for(int i = 1; i <= n; i++)
+            for(int j = 1; j <= n; j++)
+                for(int k = 1; k <= n; k++) {
+                    // 모든 자리가 주어진 첫 조합과의 거리가 2 이내인지 확인합니다.
+                    if((Math.abs(a - i) <= 2 || Math.abs(a - i) >= n - 2) && (Math.abs(b - j) <= 2 || Math.abs(b - j) >= n - 2) && 
+                       (Math.abs(c - k) <= 2 || Math.abs(c - k) >= n - 2))
+                        cnt++;
+                    
+                    // 모든 자리가 주어진 두번째 조합과의 거리가 2 이내인지 확인합니다.
+                    else if((Math.abs(a2 - i) <= 2 || Math.abs(a2 - i) >= n - 2) && (Math.abs(b2 - j) <= 2 || Math.abs(b2 - j) >= n - 2) && 
+                       (Math.abs(c2 - k) <= 2 || Math.abs(c2 - k) >= n - 2))
+                        cnt++;
                 }
-            }  
-        }
-        return count;
-    }
-
-    public static void main(String[] args)throws IOException {
-        // 여기에 코드를 작성해주세요.
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        int[] arr1 = new int[3];
-        int[] arr2 = new int[3];
-        ArrayList<int[]> arrayList1 = new ArrayList<>();
-        ArrayList<int[]> arrayList2 = new ArrayList<>();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < 3; i++) {
-            arr1[i] = Integer.parseInt(st.nextToken());
-        }
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < 3; i++) {
-            arr2[i] = Integer.parseInt(st.nextToken());
-        }
-
         
-        int sum = countfunc(arr1, arrayList1) + countfunc(arr2,arrayList2);
-        int diff = 0;
-        for(int i = 0; i < arrayList1.size(); i++) {
-            for(int j = 0; j < arrayList2.size(); j++) {
-                boolean check = true;
-                for(int k = 0; k < 3; k++) {
-                    if(arrayList1.get(i)[k] != arrayList2.get(j)[k]) {
-                        check = false;
-                        break;
-                    }
-                }
-                if(check) diff++;
-            }
-        }
-        System.out.println(sum - diff);
-
-        
-
-        
-
-
-
-        
-        
+        System.out.print(cnt);
     }
 }
